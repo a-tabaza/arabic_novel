ea278523b1402964f8882b6df1d11b754fd830b8
Abdulrahman Tabaza <nthefag@gmail.com>
2025-08-15 19:12:17 +0300

    first 22 books

---
+++ scripts/get_list.py
@@ -0,0 +1,5 @@
+import json
+import pandas as pd
+
+df = pd.read_html("https://ar.wikipedia.org/wiki/%D9%82%D8%A7%D8%A6%D9%85%D8%A9_%D8%A3%D9%81%D8%B6%D9%84_%D9%85%D8%A6%D8%A9_%D8%B1%D9%88%D8%A7%D9%8A%D8%A9_%D8%B9%D8%B1%D8%A8%D9%8A%D8%A9")
+df[2].to_csv("arabic_novels.csv")
